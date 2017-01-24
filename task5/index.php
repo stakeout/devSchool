5fee30113f14781e638499bb946bd6676a704d5f
Vadim Novash <novash@tut.by>
2017-01-24 21:41:50 +0300

    task 5 seems ready

--- task5/index.php
+++ task5/index.php
@@ -1,35 +1,53 @@
 <?php
 require_once ('base.php');
-echo '<hr>';
-echo '<pre>';
-echo $news[$_GET['id']];
-print_r($news);
-echo $news[4];
-echo '</pre>';
 function all_news_parse($arr)
 {
     if(isset($arr) && array_filter($arr)){
-        echo "<li> Список всех новостей </li>";
+        echo "<h2> Список всех новостей </h2>"
+            . '<ul>';
         foreach($arr as $key => $value){
             echo '<li>' .$value. '</li>';
         }
+        echo '</ul>';
     }else{
         return false;
     }
 }
-$id = $_GET['id'];
-function news_change_by_number($arr, $number)
+function news_by_number($arr, $id)
 {
-    if(isset($arr) && array_filter($arr) && $number){
-        if($number <= count($arr) - 1){
-            echo $arr[$number];
-        }
+    echo "<h2> Локальная новость </h2>";
+    echo '<p>' .$arr[$id]. '</p>';
+}
+$post_id = $_POST['id'];
+$get_id = $_GET['id'];
+$id ='';
+if(empty($post_id)){
+    $id = $get_id;
+}else{
+    $id = $post_id;
+}
+if(empty($id)){
+    all_news_parse($news);
+}else{
+    if($id <= count($news) - 1){
+        news_by_number($news, $id);
     }else{
-        header('HTTP/1.0 404 Not Found', true, 404);
+        header("HTTP/1.0 404 Not Found");
+        echo "<h1>Ошибка 404</h1>";
     }
 }
 ?>
-<ul>
-    <?php all_news_parse($news); ?>
-</ul>
-<p><?php news_change_by_number($news, $id)?></p>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Метод POST</title>
+</head>
+<body>
+<form method="post" action="index.php">
+    <input type="text" placeholder="Введите цифру" name="id" pattern="[0-9]{1,2}">
+    <button type="submit">Перейти к новости</button>
+</form>
+<?php echo $id; ?>
+</body>
+</html>
