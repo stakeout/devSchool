9b8913001bd7493c900b7e05cb4ff833eb077362
Vadim Novash <novash@tut.by>
2017-01-25 13:09:31 +0000

    task 5 modified

--- task5/index.php
+++ task5/index.php
@@ -20,20 +20,34 @@ function news_by_number($arr, $id)
 }
 $post_id = $_POST['id'];
 $get_id = $_GET['id'];
-$id ='';
+$id = array();
+//унифицируем переменную id для работы и с GET и с POST
 if(empty($post_id)){
-    $id = $get_id;
+    $id['id'] = $get_id;
 }else{
-    $id = $post_id;
+    $id['id'] = $post_id;
 }
+//проверяем пришедший массив, если пустой выводим все новости
 if(empty($id)){
     all_news_parse($news);
+//если массив не пустой
 }else{
-    if($id <= count($news) - 1){
-        news_by_number($news, $id);
-    }else{
+//проверяем, есть ли в нем атрибут id
+    if(array_key_exists('id', $id)){
+//если есть, проверяем чтобы полученное значение было в массиве новостей и выводим соответствующую новость.
+      if($id['id'] <= count($news) - 1){
+        news_by_number($news, $id['id']);
+//Если атрибут id пустой или не входит в рендж - выводим все новости
+      }else{
         header("HTTP/1.0 404 Not Found");
         echo "<h1>Ошибка 404</h1>";
+        echo "<p>Такой новости нет на сайте";
+        all_news_parse($news);
+        }
+//если атрибута id в массиве нет, выводим 404 ошибку        
+    }else{
+      header("HTTP/1.0 404 Not Found");
+      echo "<h1>Ошибка 404</h1>";
     }
 }
 ?>
@@ -48,6 +62,6 @@ if(empty($id)){
     <input type="text" placeholder="Введите цифру" name="id" pattern="[0-9]{1,2}">
     <button type="submit">Перейти к новости</button>
 </form>
-<?php echo $id; ?>
+<?php echo $id['id']; ?>
 </body>
 </html>
